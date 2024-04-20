dcfa9a257e825a3f5889bad84f3a8fd3783e8e1a
Cristobal Barberis <cristobal@barberis.com>
2024-04-19 23:59:12 -0400

    container django nginx psql

---
+++ app/fossil/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fossil project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fossil.settings')
+
+application = get_asgi_application()
