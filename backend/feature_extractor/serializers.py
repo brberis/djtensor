542e9d053c9945b532dd6a52948f0948aced5e05
Cristobal Barberis <cristobal@barberis.com>
2024-04-29 23:32:04 -0400

    training tab

--- backend/feature_extractor/serializers.py
+++ backend/feature_extractor/serializers.py
@@ -1,15 +1,30 @@
 from rest_framework import serializers
 from .models import TFModel, TrainingSession, Epoch
-
+from datasets.models import Dataset
+from datasets.serializers import DatasetSerializer  
 class TFModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = TFModel
         fields = '__all__'
 
 class TrainingSessionSerializer(serializers.ModelSerializer):
+    model = TFModelSerializer(read_only=True)
+    model_id = serializers.PrimaryKeyRelatedField(
+        queryset=TFModel.objects.all(),
+        source='model',
+        write_only=True
+    )
+    dataset = DatasetSerializer(read_only=True)
+    dataset_id = serializers.PrimaryKeyRelatedField(
+        queryset=Dataset.objects.all(),
+        source='dataset',
+        write_only=True
+    )
+
+
     class Meta:
         model = TrainingSession
-        fields = '__all__'
+        fields = ['id', 'name', 'notes', 'status', 'model', 'model_id', 'dataset', 'dataset_id', 'created_at', 'updated_at']
 
 class EpochSerializer(serializers.ModelSerializer):
     class Meta:
--- frontend/app/components/Layout/index.js
+++ frontend/app/components/Layout/index.js
@@ -1,23 +1,30 @@
 import Navbar from "../Navbar";
 import Header from "../Header";
 import Footer from "../Footer";
+import Sidebar from "../Sidebar";
 import { useRouter } from 'next/router';
 
-const Layout = ({ children, sideBarNav, sectionTitle }) => {
+const Layout = (props) => {
   const router = useRouter();
+  const { children, sectionTitle, action, actions, incomingAction, title, breadcrumbs, sideBarNav } = props;
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
 
   return (
     <div className="w-full">
-      <Navbar />
-      <div className={sideBarNav ? "flex" : ""}>
-        <div className="flex-auto max-w-7xl sm:px-6 lg:px-8 mb-3">
-          {/* <Header sectionTitle={sectionTitle} /> */}
+      <Navbar/>
+      <div className={classNames(sideBarNav && "flex")}>
+        {sideBarNav && <Sidebar navigation={sideBarNav}/>}
+        <div className={classNames("flex-auto", "max-w-7xl sm:px-6 lg:px-8 mb-3")}>
+          <Header sectionTitle={sectionTitle} action={action} actions={actions} incomingAction={incomingAction} breadcrumbs={breadcrumbs} title={title} />
           {children}
         </div>
       </div>
       <Footer />
     </div>
-  );
+  )
 }
 
 export default Layout;
