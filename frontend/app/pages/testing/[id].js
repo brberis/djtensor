6fe809d82d11d6477b18b3c8e36923289a818187
Cristobal Barberis <cristobal@barberis.com>
2024-05-09 23:48:16 -0400

    test front end no image

--- frontend/app/pages/testing/[id].js
+++ frontend/app/pages/testing/[id].js
@@ -50,6 +50,10 @@ export default function TestDetail() {
     fetchData();
   }, [id]);
 
+  function formatLabel(label) {
+    return label.replace(/_/g, ' ').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
+
   // useEffect(() => {
   //   if (!isLoading && confusionMatrix.length) {
   //     const data = { values: confusionMatrix };
@@ -74,13 +78,10 @@ export default function TestDetail() {
         <div className="mt-8">
           {testResults?.length > 0 ? testResults.map(result => (
             <div key={result.id} className="bg-white shadow overflow-hidden sm:rounded-lg p-4 mb-4 flex">
-              <div className="mr-4">
-                <img src={result.image?.image} alt="Test Image" className="w-32 h-32" />
-                <p className="text-xs text-gray-500 text-center">{result.image?.label?.name}</p> {/* Label text added here */}
-              </div>
               <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">Prediction: {result.prediction?.name}</h3>
-                <p className="text-sm text-gray-500">Confidence: {result.confidence.toFixed(2)}%</p>
+              <h3 className="text-lg leading-6 font-medium text-gray-900">Prediction: {formatLabel(result.prediction)}</h3>
+              <h3 className="text-md leading-6 font-medium text-gray-900">True Label: {formatLabel(result.true_label)}</h3>
+                <p className="text-md text-gray-800">Confidence: {result.confidence.toFixed(2) * 100}%</p>
               </div>
             </div>
           )) : (
