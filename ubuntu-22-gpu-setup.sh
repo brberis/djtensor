a8a1d3990257f4463033a97c4100d8b388cd2765
Cristobal Barberis <cristobal@barberis.com>
2024-07-07 23:16:36 -0400

    fix db yml

--- ubuntu-22-gpu-setup.sh
+++ ubuntu-22-gpu-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Function to generate a random Django SECRET_KEY
+# Function to generate a random Django SECRET_KEY without using Django
 generate_secret_key() {
-  python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+  python3 -c "import secrets; print(''.join([secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)]))"
 }
 
 # Check if at least two arguments are provided
@@ -20,10 +20,10 @@ if lspci | grep -i nvidia > /dev/null; then
   echo "NVIDIA GPU detected. Installing NVIDIA driver and CUDA toolkit."
 
   # Add NVIDIA package repositories
-  sudo apt-get update
+  sudo apt-get update -y
   sudo apt-get install -y software-properties-common
-  sudo add-apt-repository ppa:graphics-drivers/ppa
-  sudo apt-get update
+  sudo add-apt-repository ppa:graphics-drivers/ppa -y
+  sudo apt-get update -y
 
   # Install the appropriate NVIDIA driver
   sudo apt-get install -y nvidia-driver-530
@@ -47,7 +47,7 @@ else
 fi
 
 # Update package list and install prerequisites
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -61,16 +61,16 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable" -y
 
 # Update package list again to include Docker's packages
-sudo apt-get update
+sudo apt-get update -y
 
 # Install Docker CE
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Add the package repositories for NVIDIA Container Toolkit
@@ -80,7 +80,7 @@ if nvidia-smi > /dev/null 2>&1; then
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
     # Update the package lists and install the NVIDIA Container Toolkit
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install -y nvidia-docker2
 
     # Restart the Docker daemon to complete the installation
@@ -116,4 +116,4 @@ docker-compose --version
 echo "Docker and Docker Compose installation completed. Please log out and log back in for the changes to take effect."
 
 # Run Docker Compose to build and start the services
-docker-compose -f docker-compose.prod.yml up --build -d
+sudo docker-compose -f docker-compose.prod.yml up --build
