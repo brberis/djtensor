f5bb5588ce46f2d4bde0f244fef2e4ad0715440f
Cristobal Barberis <cristobal@barberis.com>
2024-07-08 08:17:53 -0400

    migration script on compose yml

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -3,6 +3,10 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
+    echo "SQL_HOST=$SQL_HOST"
+    echo "SQL_PORT=$SQL_PORT"
+    echo "SQL_USER=$SQL_USER"
+    echo "SQL_DATABASE=$SQL_DATABASE"
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
@@ -11,8 +15,8 @@ then
     echo "PostgreSQL started"
 
     # Create the database if it doesn't exist
-    psql -h $SQL_HOST -U $SQL_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$SQL_DATABASE'" | grep -q 1 || \
-    psql -h $SQL_HOST -U $SQL_USER -c "CREATE DATABASE $SQL_DATABASE"
+    PGPASSWORD=$SQL_PASSWORD psql -h $SQL_HOST -U $SQL_USER -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$SQL_DATABASE'" | grep -q 1 || \
+    PGPASSWORD=$SQL_PASSWORD psql -h $SQL_HOST -U $SQL_USER -d postgres -c "CREATE DATABASE $SQL_DATABASE"
 fi
 
 exec "$@"
