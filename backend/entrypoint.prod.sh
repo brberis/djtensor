63beae5c43baf6a743d2fb6b8194ccebb2493401
Cristobal Barberis <cristobal@barberis.com>
2024-07-07 23:33:26 -0400

    fixed entrypoint for creating db

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -9,6 +9,10 @@ then
     done
 
     echo "PostgreSQL started"
+
+    # Create the database if it doesn't exist
+    psql -h $SQL_HOST -U $SQL_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$SQL_DATABASE'" | grep -q 1 || \
+    psql -h $SQL_HOST -U $SQL_USER -c "CREATE DATABASE $SQL_DATABASE"
 fi
 
 exec "$@"
