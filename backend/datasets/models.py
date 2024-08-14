13dfa194c2dfdb2428d761d70f9477718e344832
Cristobal Barberis <cristobal@barberis.com>
2024-08-14 17:19:51 -0400

    allow null in shared datasets

--- backend/datasets/models.py
+++ backend/datasets/models.py
@@ -18,7 +18,7 @@ class Dataset(models.Model):
     resolution = models.CharField(max_length=10, choices=RESOLUTIONS, default='224')
     base = models.BooleanField(default=False)
     for_testing = models.BooleanField(default=True)
-    shared = models.ManyToManyField('feature_extractor.Study', related_name='shared_datasets', blank=True)
+    shared = models.ManyToManyField('feature_extractor.Study', related_name='shared_datasets', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
