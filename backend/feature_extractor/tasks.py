62adc304bf57b65ea21a8a3ff62c553fa8b71b67
Ubuntu <cristobal@barberis.com>
2024-09-08 03:37:14 +0000

    remove background before training

--- backend/feature_extractor/tasks.py
+++ backend/feature_extractor/tasks.py
@@ -9,6 +9,7 @@ import tensorflow_hub as hub
 from tf_keras_vis.utils.scores import CategoricalScore
 from tf_keras_vis.utils.model_modifiers import ReplaceToLinear
 from tf_keras_vis.saliency import Saliency
+from .model_config import model_handle_map, model_image_size_map
 from django.core.files import File
 from django.utils.text import get_valid_filename
 from uuid import uuid4
@@ -65,101 +66,8 @@ def train_model(training_session_id, *args, **kwargs):
         #@title
         # Selection of the pre train model  
         
-
         model_name = model_instance.pre_model
         
-        model_handle_map = {
-        "efficientnetv2-s": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_s/feature_vector/2",
-        "efficientnetv2-m": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_m/feature_vector/2",
-        "efficientnetv2-l": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_l/feature_vector/2",
-        "efficientnetv2-s-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_s/feature_vector/2",
-        "efficientnetv2-m-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_m/feature_vector/2",
-        "efficientnetv2-l-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_l/feature_vector/2",
-        "efficientnetv2-xl-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_xl/feature_vector/2",
-        "efficientnetv2-b0-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b0/feature_vector/2",
-        "efficientnetv2-b1-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b1/feature_vector/2",
-        "efficientnetv2-b2-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b2/feature_vector/2",
-        "efficientnetv2-b3-21k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_b3/feature_vector/2",
-        "efficientnetv2-s-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_s/feature_vector/2",
-        "efficientnetv2-m-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_m/feature_vector/2",
-        "efficientnetv2-l-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_l/feature_vector/2",
-        "efficientnetv2-xl-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_xl/feature_vector/2",
-        "efficientnetv2-b0-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_b0/feature_vector/2",
-        "efficientnetv2-b1-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_b1/feature_vector/2",
-        "efficientnetv2-b2-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_b2/feature_vector/2",
-        "efficientnetv2-b3-21k-ft1k": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_b3/feature_vector/2",
-        "efficientnetv2-b0": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b0/feature_vector/2",
-        "efficientnetv2-b1": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b1/feature_vector/2",
-        "efficientnetv2-b2": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b2/feature_vector/2",
-        "efficientnetv2-b3": "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b3/feature_vector/2",
-        "efficientnet_b0": "https://tfhub.dev/tensorflow/efficientnet/b0/feature-vector/1",
-        "efficientnet_b1": "https://tfhub.dev/tensorflow/efficientnet/b1/feature-vector/1",
-        "efficientnet_b2": "https://tfhub.dev/tensorflow/efficientnet/b2/feature-vector/1",
-        "efficientnet_b3": "https://tfhub.dev/tensorflow/efficientnet/b3/feature-vector/1",
-        "efficientnet_b4": "https://tfhub.dev/tensorflow/efficientnet/b4/feature-vector/1",
-        "efficientnet_b5": "https://tfhub.dev/tensorflow/efficientnet/b5/feature-vector/1",
-        "efficientnet_b6": "https://tfhub.dev/tensorflow/efficientnet/b6/feature-vector/1",
-        "efficientnet_b7": "https://tfhub.dev/tensorflow/efficientnet/b7/feature-vector/1",
-        "bit_s-r50x1": "https://tfhub.dev/google/bit/s-r50x1/1",
-        "inception_v3": "https://tfhub.dev/google/imagenet/inception_v3/feature-vector/4",
-        "inception_resnet_v2": "https://tfhub.dev/google/imagenet/inception_resnet_v2/feature-vector/4",
-        "resnet_v1_50": "https://tfhub.dev/google/imagenet/resnet_v1_50/feature-vector/4",
-        "resnet_v1_101": "https://tfhub.dev/google/imagenet/resnet_v1_101/feature-vector/4",
-        "resnet_v1_152": "https://tfhub.dev/google/imagenet/resnet_v1_152/feature-vector/4",
-        "resnet_v2_50": "https://tfhub.dev/google/imagenet/resnet_v2_50/feature-vector/4",
-        "resnet_v2_101": "https://tfhub.dev/google/imagenet/resnet_v2_101/feature-vector/4",
-        "resnet_v2_152": "https://tfhub.dev/google/imagenet/resnet_v2_152/feature-vector/4",
-        "nasnet_large": "https://tfhub.dev/google/imagenet/nasnet_large/feature_vector/4",
-        "nasnet_mobile": "https://tfhub.dev/google/imagenet/nasnet_mobile/feature_vector/4",
-        "pnasnet_large": "https://tfhub.dev/google/imagenet/pnasnet_large/feature_vector/4",
-        "mobilenet_v2_100_224": "https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/4",
-        "mobilenet_v2_130_224": "https://tfhub.dev/google/imagenet/mobilenet_v2_130_224/feature_vector/4",
-        "mobilenet_v2_140_224": "https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/feature_vector/4",
-        "mobilenet_v3_small_100_224": "https://tfhub.dev/google/imagenet/mobilenet_v3_small_100_224/feature_vector/5",
-        "mobilenet_v3_small_075_224": "https://tfhub.dev/google/imagenet/mobilenet_v3_small_075_224/feature_vector/5",
-        "mobilenet_v3_large_100_224": "https://tfhub.dev/google/imagenet/mobilenet_v3_large_100_224/feature_vector/5",
-        "mobilenet_v3_large_075_224": "https://tfhub.dev/google/imagenet/mobilenet_v3_large_075_224/feature_vector/5",
-        }
-
-        model_image_size_map = {
-        "efficientnetv2-s": 384,
-        "efficientnetv2-m": 480,
-        "efficientnetv2-l": 480,
-        "efficientnetv2-b0": 224,
-        "efficientnetv2-b1": 240,
-        "efficientnetv2-b2": 260,
-        "efficientnetv2-b3": 300,
-        "efficientnetv2-s-21k": 384,
-        "efficientnetv2-m-21k": 480,
-        "efficientnetv2-l-21k": 480,
-        "efficientnetv2-xl-21k": 512,
-        "efficientnetv2-b0-21k": 224,
-        "efficientnetv2-b1-21k": 240,
-        "efficientnetv2-b2-21k": 260,
-        "efficientnetv2-b3-21k": 300,
-        "efficientnetv2-s-21k-ft1k": 384,
-        "efficientnetv2-m-21k-ft1k": 480,
-        "efficientnetv2-l-21k-ft1k": 480,
-        "efficientnetv2-xl-21k-ft1k": 512,
-        "efficientnetv2-b0-21k-ft1k": 224,
-        "efficientnetv2-b1-21k-ft1k": 240,
-        "efficientnetv2-b2-21k-ft1k": 260,
-        "efficientnetv2-b3-21k-ft1k": 300, 
-        "efficientnet_b0": 224,
-        "efficientnet_b1": 240,
-        "efficientnet_b2": 260,
-        "efficientnet_b3": 300,
-        "efficientnet_b4": 380,
-        "efficientnet_b5": 456,
-        "efficientnet_b6": 528,
-        "efficientnet_b7": 600,
-        "inception_v3": 299,
-        "inception_resnet_v2": 299,
-        "nasnet_large": 331,
-        "pnasnet_large": 331,
-        }
-
-
         model_handle = model_handle_map.get(model_name)
         pixels = model_image_size_map.get(model_name, 224)
 
@@ -194,15 +102,23 @@ def train_model(training_session_id, *args, **kwargs):
 
         logger.info("Building and compiling model...")
 
-        def remove_alpha_and_background(image):
-            """Remove alpha channel and replace transparency with white background."""
-            if image.shape[-1] == 4:  # Check if image has an alpha channel
-                image_pil = PILImage.fromarray((image * 255).astype(np.uint8))  # Convert to PIL image
-                background = PILImage.new("RGB", image_pil.size, (255, 255, 255))
-                background.paste(image_pil, mask=image_pil.split()[3])  # Paste with alpha channel as mask
-                return np.array(background) / 255.0  # Convert back to numpy array and normalize
-            return image
-        
+        def convert_alpha_to_white(image_path):
+            """Converts an image with alpha transparency to a white background and saves it in the same location."""
+            image = PILImage.open(image_path).convert("RGBA")
+            background = PILImage.new("RGB", image.size, (255, 255, 255)) 
+            background.paste(image, mask=image.split()[3])  
+            background.save(image_path, "PNG")  
+
+        def preprocess_dataset_images(data_dir):
+            """Iterates through all images in the data directory and converts them to remove transparency, saving in-place."""
+            for root, _, files in os.walk(data_dir):
+                for file in files:
+                    if file.endswith(".png") or file.endswith(".jpg"):
+                        file_path = os.path.join(root, file)
+                        convert_alpha_to_white(file_path)
+
+        preprocess_dataset_images(data_dir)
+
         def build_dataset(subset):
             return tf.keras.preprocessing.image_dataset_from_directory(
                 data_dir,
@@ -211,7 +127,8 @@ def train_model(training_session_id, *args, **kwargs):
                 label_mode="categorical",
                 seed=123,
                 image_size=IMAGE_SIZE,
-                batch_size=1)
+                batch_size=1
+            )
 
         train_ds = build_dataset("training")
         class_names = tuple(train_ds.class_names)
@@ -244,10 +161,8 @@ def train_model(training_session_id, *args, **kwargs):
 
         def save_image(image_array, file_name):
             """Save a NumPy array as an image file in the Django media directory."""
-            # Assuming the image values are in the range [0, 1], rescale to [0, 255]
             rescaled_image = np.clip(image_array * 255, 0, 255).astype(np.uint8)
 
-            # Convert the NumPy array to a PIL image and save it
             image = PILImage.fromarray(rescaled_image)
             image_path = os.path.join(settings.MEDIA_ROOT, 'augmented_images', file_name)
             image.save(image_path)
